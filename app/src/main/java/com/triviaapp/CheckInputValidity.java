e7b31d6eb2ff404fe7d26bb4adbbec83840b1c3b
confuzzed03 <kim.4256@osu.edu>
2016-04-12 00:04:36 -0400

    Fixed merging
    Fixed merging

--- app/src/main/java/com/triviaapp/CheckInputValidity.java
+++ app/src/main/java/com/triviaapp/CheckInputValidity.java
@@ -1,117 +1,114 @@
 package com.triviaapp;
 
 /**
-<<<<<<< Updated upstream
- * Created by Di on 4/11/16.
- */
-public class CheckInputValidity {
-=======
  * Created by yu2749luca on 4/11/16.
  */
 public class CheckInputValidity {
 
->>>>>>> Stashed changes
-    int asiccCeiling=126;
-    int asiccRange=95;
-    int asiccFloor=32;
+    int asiccCeiling = 126;
+    int asiccRange = 95;
+    int asiccFloor = 32;
 
-    int firstQ=58;
-    int lastQ=62;
+    int firstQ = 58;
+    int lastQ = 62;
 
-    private boolean checkAsicc=true;
-    private boolean checkLength=true;
-    private boolean checkDrop=true;
-    private boolean checkEquation=true;
-    private boolean checkAt=true;
-    private boolean checkCompare=true;
+    private boolean checkAsicc = true;
+    private boolean checkLength = true;
+    private boolean checkDrop = true;
+    private boolean checkEquation = true;
+    private boolean checkAt = true;
+    private boolean checkCompare = true;
 
-    private boolean overall=false;
-    private boolean checkPassword=false;
+    private boolean overall = false;
+    private boolean checkPassword = false;
 
     public String testString = "ALL TRUE";
-    public CheckInputValidity(){
 
+    public CheckInputValidity() {
 
 
     }
-    public boolean overallCheck (String input){
-        checkAsicc=checkAsicc(input);
-        checkLength=checkLength(input);
-        checkDrop=checkDrop(input);
-        checkEquation=checkEquation(input);
-        checkAt=checkAt(input);
-        checkCompare=checkCompare(input);
-        overall=(checkAsicc)&&(checkLength)&&(checkLength)&&(checkDrop)&&(checkEquation)&&(checkAt)&&(checkCompare);
-        if (!overall){
-            testString="FALSE";
+
+    public boolean overallCheck(String input) {
+        checkAsicc = checkAsicc(input);
+        checkLength = checkLength(input);
+        checkDrop = checkDrop(input);
+        checkEquation = checkEquation(input);
+        checkAt = checkAt(input);
+        checkCompare = checkCompare(input);
+        overall = (checkAsicc) && (checkLength) && (checkLength) && (checkDrop) && (checkEquation) && (checkAt) && (checkCompare);
+        if (!overall) {
+            testString = "FALSE";
         }
         return overall;
     }
 
     public boolean checkPassword(String input) {
-        checkAsicc=checkAsicc(input);
-        checkLength=checkLength(input);
-        if ((checkAsicc)&&(checkLength)){
-            checkPassword=true;
+        checkAsicc = checkAsicc(input);
+        checkLength = checkLength(input);
+        if ((checkAsicc) && (checkLength)) {
+            checkPassword = true;
         }
 
         return checkPassword;
     }
-    public String alterMessage (String input){
-        String alterMessage=input;
+
+    public String alterMessage(String input) {
+        String alterMessage = input;
 
 
         return alterMessage;
     }
 
-    public boolean checkAsicc (String input){
+    public boolean checkAsicc(String input) {
 
 
-        for (int i=0;i<input.length();i++){
-            char c=input.charAt(i);
-            if ((c>asiccCeiling)||(c<asiccFloor)){
-                checkAsicc=false;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if ((c > asiccCeiling) || (c < asiccFloor)) {
+                checkAsicc = false;
             }
         }
         return checkAsicc;
 
     }
-    private boolean checkLength(String input){
-        if (input.length()<2){
-            checkLength=false;
+
+    private boolean checkLength(String input) {
+        if (input.length() < 2) {
+            checkLength = false;
         }
 
         return checkLength;
     }
 
-    private boolean checkDrop(String input){
+    private boolean checkDrop(String input) {
 
-        String message= input.replace(" ","");
-        message=message.toLowerCase();
-        String check=";droptable";
-        if (message.contains(check)){checkDrop=false;}
+        String message = input.replace(" ", "");
+        message = message.toLowerCase();
+        String check = ";droptable";
+        if (message.contains(check)) {
+            checkDrop = false;
+        }
 
 
         return checkDrop;
     }
 
-    private boolean checkEquation(String input){
+    private boolean checkEquation(String input) {
 
 
+        String message = input.replace(" ", "");
+        message = message.toLowerCase();
 
+        for (int i = 1; i < message.length() - 1; i++) {
+            char c = message.charAt(i);
+            int cV = (int) c;
+            if ((cV >= firstQ) && (cV <= lastQ)) {
+                char checkPrevious = message.charAt(i - 1);
+                char checkAfter = message.charAt(i + 1);
 
-        String message= input.replace(" ","");
-        message=message.toLowerCase();
-
-        for (int i=1;i<message.length()-1;i++){
-            char c=message.charAt(i);
-            int cV= (int)c;
-            if ((cV>=firstQ)&&(cV<=lastQ)){
-                char checkPrevious=message.charAt(i - 1);
-                char checkAfter=message.charAt(i + 1);
-
-                if ((Character.isDigit(checkPrevious)&&(Character.isDigit(checkAfter)))){
-                    checkEquation=false;
+                if ((Character.isDigit(checkPrevious) && (Character.isDigit(checkAfter)))) {
+                    checkEquation = false;
                     i = message.length();
                 }
 
@@ -123,21 +120,26 @@ public class CheckInputValidity {
         return checkEquation;
     }
 
-    private boolean checkCompare (String input){
-        boolean checkSigns=false;
-        String message=input.replace(" ","");
-        message=message.toLowerCase();
-        for (int i =2;i<message.length()-1;i++){
+    private boolean checkCompare(String input) {
+        boolean checkSigns = false;
+        String message = input.replace(" ", "");
+        message = message.toLowerCase();
+        for (int i = 2; i < message.length() - 1; i++) {
 
-            if(message.charAt(i)=='=') {
-                char previous2=message.charAt(i-2);
-                char previous=message.charAt(i-1);
-                char after=message.charAt(i+1);
-                boolean checkFrontDigit=Character.isDigit(previous2);
-                boolean checkAfterDigit=Character.isDigit(after);
+            if (message.charAt(i) == '=') {
+                char previous2 = message.charAt(i - 2);
+                char previous = message.charAt(i - 1);
+                char after = message.charAt(i + 1);
+                boolean checkFrontDigit = Character.isDigit(previous2);
+                boolean checkAfterDigit = Character.isDigit(after);
 
-                if ((previous=='!')||(previous=='>')||(previous=='<')){checkSigns=true;}
-                if (checkAfterDigit&&checkFrontDigit&&checkSigns){checkCompare=false;i=message.length();}
+                if ((previous == '!') || (previous == '>') || (previous == '<')) {
+                    checkSigns = true;
+                }
+                if (checkAfterDigit && checkFrontDigit && checkSigns) {
+                    checkCompare = false;
+                    i = message.length();
+                }
             }
 
 
@@ -147,19 +149,19 @@ public class CheckInputValidity {
         return checkCompare;
     }
 
-    private boolean checkAt(String input){
+    private boolean checkAt(String input) {
 
-        String message= input.replace(" ","");
-        message=message.toLowerCase();
-        String check="@";
+        String message = input.replace(" ", "");
+        message = message.toLowerCase();
+        String check = "@";
 
-        for (int i=0;i<message.length()-1;i++){
-            char c=message.charAt(i);
-            if (c=='@'){
-                char after = message.charAt(i+1);
-                if(Character.isDigit(after)){
-                    checkAt=!Character.isDigit(after);
-                    i=message.length();
+        for (int i = 0; i < message.length() - 1; i++) {
+            char c = message.charAt(i);
+            if (c == '@') {
+                char after = message.charAt(i + 1);
+                if (Character.isDigit(after)) {
+                    checkAt = !Character.isDigit(after);
+                    i = message.length();
                 }
 
             }
@@ -171,9 +173,4 @@ public class CheckInputValidity {
         return checkAt;
     }
 
-
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 }
--- app/src/main/java/com/triviaapp/Encryption.java
+++ app/src/main/java/com/triviaapp/Encryption.java
@@ -6,67 +6,67 @@ package com.triviaapp;
 
 public class Encryption {
 
-    int asiccCeiling=126;
-    int asiccRange=95;
-    int asiccFloor=32;
-    int [] pattern;
+    int asiccCeiling = 126;
+    int asiccRange = 95;
+    int asiccFloor = 32;
+    int[] pattern;
 
     int passLength;
     int special1;
     int special2;
     int special3;
     int special4;
-    int andyAge=22;
-    int [] blocks;
+    int andyAge = 22;
+    int[] blocks;
 
-    String tail="";
+    String tail = "";
 
-    public Encryption (String password){
+    public Encryption(String password) {
         insertPassword(password);
         arrangePattern(password);
         tail(password);
     }
 
-    public String encrypt (String message){
-        assert message.length()>0:"error, message not exist";
+    public String encrypt(String message) {
+        assert message.length() > 0 : "error, message not exist";
 
-        String encryptM="";
-        int count=0;
-        char changedC=' ';
+        String encryptM = "";
+        int count = 0;
+        char changedC = ' ';
         //System.out.print("block encrypt ");
-        while (!message.isEmpty()){
-            if (count>pattern.length-1){
-                count=count-pattern.length;
+        while (!message.isEmpty()) {
+            if (count > pattern.length - 1) {
+                count = count - pattern.length;
             }
-            int patternV=pattern[count];
-            int blockN=blocks [patternV];
+            int patternV = pattern[count];
+            int blockN = blocks[patternV];
             //System.out.print(" "+blockN);
-            changedC=changeC(message.charAt(0),blockN);
-            encryptM=encryptM+changedC;
-            message=message.substring(1);
+            changedC = changeC(message.charAt(0), blockN);
+            encryptM = encryptM + changedC;
+            message = message.substring(1);
             count++;
 
         }
         //System.out.println();
-        return encryptM+tail;
+        return encryptM + tail;
     }
 
-    public String decrypt(String message){
-        String decryptM="";
-        int count=0;
-        char returnC=' ';
-        message=message.substring(0, message.length()-special4);
+    public String decrypt(String message) {
+        String decryptM = "";
+        int count = 0;
+        char returnC = ' ';
+        message = message.substring(0, message.length() - special4);
         //System.out.print("block decrypt ");
-        while (!message.isEmpty()){
-            if (count>pattern.length-1){
-                count=count-pattern.length;
+        while (!message.isEmpty()) {
+            if (count > pattern.length - 1) {
+                count = count - pattern.length;
             }
-            int patternV=pattern[count];
-            int blockN=blocks [patternV];
+            int patternV = pattern[count];
+            int blockN = blocks[patternV];
             //System.out.print(" "+blockN);
-            returnC=returnC(message.charAt(0),blockN);
-            decryptM=decryptM+returnC;
-            message=message.substring(1);
+            returnC = returnC(message.charAt(0), blockN);
+            decryptM = decryptM + returnC;
+            message = message.substring(1);
             count++;
 
         }
@@ -74,93 +74,82 @@ public class Encryption {
         return decryptM;
     }
 
-    private void tail (String password){
+    private void tail(String password) {
 
-        for (int i=0;i<special4;i++){
-            double drandom= Math.random()*30;
-            int intRan=(int) drandom;
-            char addC=changeC('a',intRan);
-            tail=tail+addC;
+        for (int i = 0; i < special4; i++) {
+            double drandom = Math.random() * 30;
+            int intRan = (int) drandom;
+            char addC = changeC('a', intRan);
+            tail = tail + addC;
         }
     }
 
     //each position in the array determines how much a character is shifted
     //eventually pass the password on to set the pattern
-    public void insertPassword (String password){
-        passLength=password.length();
-        int passwordValue=passwordValue(password);
+    public void insertPassword(String password) {
+        passLength = password.length();
+        int passwordValue = passwordValue(password);
 
-        special1=password.length()+passwordValue;
-        special2=passwordValue;
-        String allCap=password.toUpperCase();
-        int allCapValue=passwordValue(allCap);
-        special3=allCapValue;
-        special4=special1%passLength+1;
+        special1 = password.length() + passwordValue;
+        special2 = passwordValue;
+        String allCap = password.toUpperCase();
+        int allCapValue = passwordValue(allCap);
+        special3 = allCapValue;
+        special4 = special1 % passLength + 1;
 
-        this.blocks=new int [] {special1,special3,andyAge,special2,passLength,special4};
+        this.blocks = new int[]{special1, special3, andyAge, special2, passLength, special4};
         //this.blocks=new int [] {1,2,3,4,5,6};
     }
 
-    private void arrangePattern (String password){
-        int size=2;
-        if (password.length()>size){
-            size=password.length();
+    private void arrangePattern(String password) {
+        int size = 2;
+        if (password.length() > size) {
+            size = password.length();
         }
-        pattern=new int [size];
-        int passwordValue=passwordValue(password);
+        pattern = new int[size];
+        int passwordValue = passwordValue(password);
 
-        for (int i=0;i<pattern.length;i++){
-            int wordValue=(int) password.charAt(i)+passwordValue;
-            int remain=wordValue%blocks.length;
-            pattern [i]=remain;
+        for (int i = 0; i < pattern.length; i++) {
+            int wordValue = (int) password.charAt(i) + passwordValue;
+            int remain = wordValue % blocks.length;
+            pattern[i] = remain;
         }
         //pattern=new int [] {1,2,4,5,3};
         //System.out.println("pattern :"+pattern [2]);
     }
-    private int passwordValue (String password){
-        int value=0;
-        while (!password.isEmpty()){
-            value=value+(int)password.charAt(0);
-            password=password.substring(1);
+
+    private int passwordValue(String password) {
+        int value = 0;
+        while (!password.isEmpty()) {
+            value = value + (int) password.charAt(0);
+            password = password.substring(1);
         }
 
         return value;
     }
 
 
-    private char changeC (char messageC, int shifted){
+    private char changeC(char messageC, int shifted) {
 
-        int asiccCode=(int) messageC+shifted;
+        int asiccCode = (int) messageC + shifted;
 
-        while (asiccCode>asiccCeiling){
-            asiccCode=asiccCode-asiccRange;
+        while (asiccCode > asiccCeiling) {
+            asiccCode = asiccCode - asiccRange;
         }
-        char changeC=(char) asiccCode;
+        char changeC = (char) asiccCode;
         //System.out.print(" "+asiccCode+" ");
         return changeC;
     }
 
-    private char returnC(char messageC, int shifted){
-        int asiccCode=(int) messageC-shifted;
+    private char returnC(char messageC, int shifted) {
+        int asiccCode = (int) messageC - shifted;
 
-        while (asiccCode<asiccFloor){
-            asiccCode=asiccCode+asiccRange;
+        while (asiccCode < asiccFloor) {
+            asiccCode = asiccCode + asiccRange;
         }
 
-        char returnC=(char) asiccCode;
+        char returnC = (char) asiccCode;
         //System.out.print(" "+asiccCode+" ");
         return returnC;
     }
-<<<<<<< HEAD
-=======
-
-
-
-
-<<<<<<< Updated upstream
->>>>>>> c60b18105c457b9944cb5cd01b4cdc7fbfe08f61
 }
-
-=======
-}
->>>>>>> Stashed changes
